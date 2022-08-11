45e9133373ee5933c8dfdb323d72b6697d15be7a
manvendra428 <110771012+manvendra428@users.noreply.github.com>
2022-08-11 17:59:08 +0530

    Add files via upload

---
+++ payramid_number_triangle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class payramid_number_triangle 
+{
+public static void main(String args[])
+{
+    Scanner sc= new Scanner(System.in);
+    int row = sc.nextInt();
+    int num = 1;
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=row-i;j++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(num+" ");
+        }
+        num++;
+        System.out.println();
+    }
+}    
+}
