3c1160909d7facf5d32ee176d9424a04eec133c3
manvendra428 <110771012+manvendra428@users.noreply.github.com>
2022-08-11 16:47:17 +0530

    Add files via upload

---
+++ printing_counting_triangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class trianglecounting{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int row    = sc.nextInt();
+        int num    = 1;
+        for(int i = 1;i<=row;i++)
+        {
+            for(int j= 1;j<=i;j++)
+            {
+                System.out.print(num+" ");
+                //counting print karni h increasing by 1 everytime
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
